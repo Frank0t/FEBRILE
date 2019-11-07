6d92e9cc51480daf5a71c969d60d63640ef9c33f
Frank0t <57467146+Frank0t@users.noreply.github.com>
2019-11-06 19:54:56 -0500

    Create febrile.py

---
+++ febrile.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+print (' _______  _______ .______   .______      __   __       _______ ')
+print ('|   ____||   ____||   _  \  |   _  \    |  | |  |     |   ____|')
+print ('|  |__   |  |__   |  |_)  | |  |_)  |   |  | |  |     |  |__   ')
+print ('|   __|  |   __|  |   _  <  |      /    |  | |  |     |   __|  ')
+print ('|  |     |  |____ |  |_)  | |  |\  \----|  | |  `----.|  |____ ')
+print ('|__|     |_______||______/  | _| `._____|__| |_______||_______|')
+print ('                         PORT SCANNER')
+
+
+
+
+
+
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(5)
+
+host = input("Please enter the IP you want to scan: ")
+port = int(input("Please enter the port you want to scan: "))
+
+
+def portScanner(port):
+    if s.connect_ex((host, port)):
+        print("The port is closed")
+    else:
+        print("The port is open")
+
+portScanner(port)
